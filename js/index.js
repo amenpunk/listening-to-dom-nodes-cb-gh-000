cf6920426865f028cca04c04abc281a606d0bfb4
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-29 01:58:35 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,3 +11,23 @@ const input = document.querySelector('input');
 input.addEventListener('keydown', function(e) {
   console.log(e.key);
 });
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
+});
+
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
